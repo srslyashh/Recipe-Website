b3b54c47e50755720b141e1dea9adf8ce0a33533
Ethan Barker <ethanbarker0@gmail.com>
2023-03-18 13:36:49 -0700

    add multiple input forms for advanced search queries

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,32 +1,67 @@
 import { useSearchParams } from 'react-router-dom'
 import { useState } from 'react'
 
- import useRecipeSearch from '../data/useRecipeSearch'
- import Spinner from '../components/Spinner'
- import ErrorContainer from '../components/ErrorContainer'
- import { Banner, ContentContainer } from '../components/Webpages'
- import { PageHeader } from '../components/Texts'
- import { SearchBar } from '../components/NavBar'
- 
+import useRecipeSearch from '../data/useRecipeSearch'
+import Spinner from '../components/Spinner'
+import ErrorContainer from '../components/ErrorContainer'
+import { Banner, ContentContainer } from '../components/Webpages'
+import { SearchBar } from '../components/NavBar'
+
+// All form fields
+const formNames = ["query", "cuisine", "diet", "intolerances", 
+  "type", "includeIngredients", "excludeIngredients"]
+
+// Returns a copy of an object without any empty attirbutes SRC: https://stackoverflow.com/a/65443021
+function filterEmptyAttributes(obj) {
+  return Object.entries(obj).reduce((acc, [k, v]) => v ? {...acc, [k]:v} : acc , {})
+}
+
+// Maps a set of params to their search parm if present, otherwise and empty string
+function mapParamsToSearchParams(stateParams, searchParams) {
+  return stateParams.reduce((acc, name) => {
+    return {
+      ...acc,
+      [name]: searchParams.get(name) || ""
+    }
+  }, {})
+}
+
 function Search() {
   const [ searchParams, setSearchParams ] = useSearchParams()
-  const [ searchQuery, setSearchQuery ] = useState(searchParams.get("query") || "")
+  const [ formValues, setFormValues ] = useState(mapParamsToSearchParams(formNames, searchParams))
   const [ recipes, loading, error ] = useRecipeSearch(searchParams)
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
 
   return (
     <>
       <Banner
-            url="/media/searchBanner.jpeg"
-            title="Search recipes"/>
+        url="/media/searchBanner.jpeg"
+        title="Search recipes"/>
       <ContentContainer>
         <SearchBar>
-        <form onSubmit={e => {
-          e.preventDefault()
-          setSearchParams({ query: searchQuery })
-        }}>
-          <input value={searchQuery} placeholder="Search by Recipe Name..." onChange={e => setSearchQuery(e.target.value)} />
-          <button type="submit">&#128269;</button>
-        </form>
+          <form onSubmit={e => {
+            e.preventDefault()
+            // Keep the URL cleaner by only including non-empty form attributes
+            setSearchParams(filterEmptyAttributes(formValues))
+          }}>
+            {formNames.map(name =>(
+              <>
+                <label>
+                  {name}:
+                  <input key={name} name={name} value={formValues[name]} placeholder={name} onChange={handleInputChange} />
+                </label>
+                <br/>
+              </>
+            ))}
+            <button type="submit">&#128269;</button>
+          </form>
         </SearchBar>
 
         {error && <ErrorContainer>Error: Try double checking your spelling!</ErrorContainer>}
